c33dee9819043f6699d185ecac74c4beae79882d
harshit960 <raj.harshit960@gmail.com>
2024-11-08 10:14:28 +0530

    Enhance OBitems component to include notes in assignment message and clean up PdfExp component code for improved readability

--- frontend/src/components/PdfExp.jsx
+++ frontend/src/components/PdfExp.jsx
@@ -3,19 +3,17 @@ import jsPDF from 'jspdf';
 import html2canvas from 'html2canvas';
 
 const PdfGenerator = (props) => {
-    //   const tableRef = useRef(null);
-    const tableRef = props.tableRef;
+  //   const tableRef = useRef(null);
+  const tableRef = props.tableRef;
 
-    const exportTableAsHtml = () => {
-        const tableHTML = tableRef.current.outerHTML;
-        const tailwindCDN = `
+  const exportTableAsHtml = () => {
+    const tableHTML = tableRef.current.outerHTML;
+    const tailwindCDN = `
      <script src="https://cdn.tailwindcss.com"></script>
 
   `;
-        console.log(tableHTML);
-
-        // Create the complete HTML structure
-        const htmlContent = `
+    // Create the complete HTML structure
+    const htmlContent = `
     <html>
       <head>
         <meta charset="UTF-8">
@@ -27,30 +25,30 @@ const PdfGenerator = (props) => {
       </body>
     </html>
   `;
-        // Create a blob with the HTML content
-        const blob = new Blob([htmlContent], { type: 'text/html' });
+    // Create a blob with the HTML content
+    const blob = new Blob([htmlContent], { type: 'text/html' });
 
-        // Create a download link
-        const link = document.createElement('a');
-        link.href = URL.createObjectURL(blob);
-        link.download = 'table.html'; // Name of the file to download
+    // Create a download link
+    const link = document.createElement('a');
+    link.href = URL.createObjectURL(blob);
+    link.download = 'table.html'; // Name of the file to download
 
-        // Append the link to the body (required for Firefox)
-        document.body.appendChild(link);
+    // Append the link to the body (required for Firefox)
+    document.body.appendChild(link);
 
-        // Trigger the download
-        link.click();
+    // Trigger the download
+    link.click();
 
-        // Clean up and remove the link
-        document.body.removeChild(link);
-    };
+    // Clean up and remove the link
+    document.body.removeChild(link);
+  };
 
-    return (
-        <div>
-            <button onClick={exportTableAsHtml}>HTML Export</button>
+  return (
+    <div>
+      <button onClick={exportTableAsHtml}>HTML Export</button>
 
-        </div>
-    );
+    </div>
+  );
 };
 
 export default PdfGenerator;
